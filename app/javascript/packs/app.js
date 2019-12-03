df2fcdf3c89c09c97bcbd88580df2806d0c930b2
Hung Pham <hpham8x@gmail.com>
2019-12-03 18:01:08 +0700

    Heroku setup: make supre assest:precompile is nothing to do!!!!!!

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -14,7 +14,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import 'stylesheets/app'
+import '../stylesheets/app'
 
 import 'bootstrap'
 import '../src/movies'
