fed43ef8d487ee97e822e5ea783961e2ab290be1
Hung Pham <hpham8x@gmail.com>
2019-12-03 17:06:02 +0700

    Specs: acceptance unsigned in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@
 
 class UsersController < ApplicationController
   def authenticate
-    unless user = User.authenticate(params[:user])
-      user = User.create!(params)
+    unless user = User.authenticate(user_params)
+      user = User.create!(user_params)
     end
     session[:current_user_id] = user.id
     redirect_to root_path
@@ -12,5 +12,9 @@ class UsersController < ApplicationController
   def logout
     session[:current_user_id] = nil
     redirect_to root_path
+  end
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
---
+++ spec/acceptances/home_with_unsigned_in_user_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+describe "Unsigned User visits Home Page", type: :feature do
+  
+  it "works" do
+    movies = [1,2].map{ |x| create(:movie, title: "movie title #{x}") }
+    visit "/"
+    expect(page).to have_selector("form[action='/authenticate']")
+    movies.each do |movie|
+      expect(page).to have_content(movie.title)
+      expect(page).to have_content("Shared by: #{movie.user.email}")
+      expect(page).to have_selector("iframe[src=\"https://www.youtube.com/embed/#{movie.video_id}\"]")      
+    end
+  end
+
+  it "shows errors if users visit share page" do
+    visit new_movie_path
+    expect(page).to have_content("Please sign in.")
+  end
+
+end
