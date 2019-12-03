344ce91ce6c202af8dcc0d9888d777cf1fe2dd29
Hung Pham <hpham8x@gmail.com>
2019-12-03 13:30:24 +0700

    Handle validation error

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,9 +2,14 @@
 
 class MoviesController < ApplicationController
   before_action :login_required!, except: [:index]
-  rescue_from Yt::Errors::NoItems, InvalidYoutubeUrl, with: :show_errors
-  rescue_from InvalidYoutubeUrl, with: :show_errors
-    
+  [
+    InvalidYoutubeUrl,
+    Yt::Errors::NoItems,
+    ActiveRecord::RecordInvalid
+  ].each do |err|
+    rescue_from err, with: :show_errors
+  end
+      
   def index
     @movies = Movie.includes(:user).all
   end
@@ -13,9 +18,9 @@ class MoviesController < ApplicationController
     CreateMovieService.new(url: params[:url], user_id: current_user.id).call
     redirect_to root_path, flash: {notice: 'Thank you for your sharing'}
   end
-  
+
   private
-    def show_errors
-      redirect_to new_movie_path, flash: {error: 'Invalid Youtube URL'}
+    def show_errors (err)
+      redirect_to new_movie_path, flash: {error: err.message || 'Invalid Youtube URL'}
     end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,5 @@
 
 class Movie < ApplicationRecord
   belongs_to :user
+  validates_uniqueness_of :video_id
 end
